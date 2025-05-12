1477a1ca80f83f32ba28874d4ec3754db3b58b94
nikp12 <nik.r.pennell@gmail.com>
2025-05-12 19:06:37 -0400

    add rotation and movement

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
-    pygame.init
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
-
+        dt = clock.tick(60) / 1000.0
+        
 
 if __name__ == "__main__":
     main()
