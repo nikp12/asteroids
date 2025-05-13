28544f86563bc730500a4cbb8662caa4914a9148
nikp12 <nik.r.pennell@gmail.com>
2025-05-12 20:17:07 -0400

    fix drawable and updatable groups

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ from constants import *
 from player import *
 from circleshape import *
 
-updatable = pygame.sprite.Group()
-updatable.add(Player)
-drawable = pygame.sprite.Group()
-drawable.add(Player)
-
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player_sprite = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    updatable.add(player_sprite)
+    drawable = pygame.sprite.Group()
+    drawable.add(player_sprite)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="black")
-        updatables.update(dt)
-        drawables.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
         
